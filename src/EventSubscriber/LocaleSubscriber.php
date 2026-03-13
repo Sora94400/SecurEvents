c9b40b004f68f38ab51fe0b804aade26db5fdfd6
BenSerade <stephane.benserade@livecampus.tech>
2026-03-13 16:54:45 +0100

    Version Finale

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct(string $defaultLocale = 'fr')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+
+    
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        
+        if ($locale = $request->getSession()->get('_locale')) {
+            $request->setLocale($locale);
+        } else {
+            
+            $request->setLocale($request->getDefaultLocale());
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
